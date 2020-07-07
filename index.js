7a8c023e6e119602af84b044b3fc52c0f7ddc5f6
Briana Oniru <damioniru16@gmail.com>
2020-07-07 03:13:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ return `Welcome, ${name}. You are number ${CafeDeli.length} in line.`;
 
 function nowServing(CafeDeli) {
   if (cafeDeli.length > 0) {
+    let i = 0
+    i++;
     var serve = cafeDeli[0];
     cafeDeli.shift();
     return `Currently serving ${serve}.`;
