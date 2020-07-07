3295216b465be162edd699a40bd0199741ce36c8
Briana Oniru <damioniru16@gmail.com>
2020-07-07 02:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var CafeDeli = [];
 
-function takeANumber(position,name) {
+function takeANumber(CafeDeli,name) {
  CafeDeli.push(`${name}`);
 return `Welcome, ${name}. You are number ${CafeDeli.length} in line.`;
 }
