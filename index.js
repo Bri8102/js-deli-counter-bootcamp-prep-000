4b8e94536fa5129fb8850a4ba8c426cfda8f6f45
Briana Oniru <damioniru16@gmail.com>
2020-07-07 03:08:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ return `Welcome, ${name}. You are number ${CafeDeli.length} in line.`;
 }
 
 function nowServing(CafeDeli) {
-  let i = 1 
-  if (cafeDeli.length >= 1) {
+  if (cafeDeli.length > 0) {
+    
     return
   } 
 }
